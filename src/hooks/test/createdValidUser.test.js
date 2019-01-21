9f245ebba06c0542a47e53863e02d792e998ad58
luong.ngoc.phuc <luong.ngoc.phuc@framgia.com>
2019-01-21 10:38:31 +0700

    update unit test for createValidUser

--- src/hooks/test/createdValidUser.test.js
+++ src/hooks/test/createdValidUser.test.js
@@ -1,6 +1,8 @@
 const { validUser } = require(`../before/user/createdValidUser.js`);
+const { MISSING_USER_NAME, CONFLIC_USER_NAME } = require(`../../constants/errors.js`);
 let context;
 let hook;
+
 beforeEach(() => {
   context = {
     type: `before`,
@@ -10,21 +12,38 @@ beforeEach(() => {
       userName: 'user01'
     },
     app: {
-      service: undefined,
-      error: undefined
+      service: {},
+      error: {}
     }
   };
 });
+
 describe(`createdValidUser`, () => {
   beforeEach(() => {
     hook = validUser;
   });
   beforeEach(() => {
-    context.app.service = {
-      find: () => { }
+    context.app.service = () => {
+      return {
+        find: objQuery => {
+          if (objQuery.query.userName === 'userNameDuplicate') return { successCase: false, total: 1 };
+          else return { successCase: true };
+        }
+      };
     };
   });
+  
+  test(`should reject when userName is missing`, async () => {
+    context.data = {};
+    await expect(hook(context)).rejects.toThrow(MISSING_USER_NAME);
+  });
+
+  test(`should reject when userName is existed`, async () => {
+    context.data = { userName: `userNameDuplicate` };
+    await expect(hook(context)).rejects.toThrow(CONFLIC_USER_NAME);
+  });
+
   test(`should pass when field is valid`, async () => {
-    await expect(hook(context)).not.toBeNull();
+    await expect(hook(context)).resolves.not.toThrowError();
   });
 });
