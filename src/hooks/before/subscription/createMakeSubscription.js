6f52ae73bfe2ecf8f90834fd22ae4ab8f5c2d413
phucln <phucluongngoc@gmail.com>
2019-02-11 00:11:11 +0700

    add idea about trial_end

--- src/hooks/before/subscription/createMakeSubscription.js
+++ src/hooks/before/subscription/createMakeSubscription.js
@@ -1,28 +1,31 @@
 const stripe = require(`../../../../config/stripe.js`);
 const error = require(`../../../constants/errors`);
+
 // const subscriptionModel = {
 //   customer: `cus_tok`, // validate customer,
-//   plans: [{
-//     plan: "plan_EL7kRn7L2E2v0g"
-//   },
-//   {
-//     plan: "plan_EL7cLi63pq1PSg",
-//     quantity: 5
-//   }]
+//   plan: 'plan_ETit72Ku9xD8dH'
 // };
 
+// draft: nextMonth, nextMonth.daysInMonth() <= attendanceClosingDay; .diff
+
 const createSubscription = async context => {
-  const { data } = context;
-  const { customer, plans } = data;
-  // if (!customer || !plans || !Array.isArray(plans) || plans.length == 0) throw Error(`${error.INVALID_PARAM}`);
-  // const subObj = {
-  //   customer,
-  //   items: []
-  // };
-  // for (let i = 0; i < plans.length; i++) {
-  //   const planItem = plans[i];
-  //   subObj.items.push({ plan: planItem.plan, quantity: planItem.quantity ? planItem.quantity : 1 });
-  // }
+  const {
+    data
+  } = context;
+
+  const {
+    customer,
+    plan,
+  } = data;
+
+  if (!customer || !plan) throw Error(`${error.INVALID_PARAM}`);
+  const subObj = {
+    customer,
+    items: [{
+      plan
+    }]
+  };
+  subObj.trial_period_days = 3;
   await stripe.subscriptions.create(subObj)
     .then(sub => {
       context.result = sub;
@@ -34,4 +37,6 @@ const createSubscription = async context => {
     });
 };
 
-module.exports = { createSubscription };  
+module.exports = {
+  createSubscription
+};
